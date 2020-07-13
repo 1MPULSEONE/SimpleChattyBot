17c11b361171fd6e492410264b8d416644d296a4
1MPULSEONE <parkhomenko2049@gmail.com>
2020-07-13 23:38:58 +0600

    Main.java modified. Robot can count now.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,11 +14,21 @@ public class Main {
         System.out.printf("What a great name you have, %s! \n", username );
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        //guessing age
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
         int num3 = scanner.nextInt();
         int userAge = (num1 * 70 + num2 * 21 + num3 * 15) % 105;
-        System.out.printf("Your age is %d; that`s a good time to start programming!"
+        System.out.printf("Your age is %d; that`s a good time to start programming! \n"
                 ,userAge);
+        //counting
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int numberCount = scanner.nextInt();
+        int counter = 0;
+        while (counter <= numberCount) {
+            System.out.printf("%d!\n",counter);
+            counter++;
+        }
+        System.out.println("Completed, have a nice day!");
     }
 }
