5b8884c3aaff62d1c01dad8d188e7ba5a1684b57
1MPULSEONE <parkhomenko2049@gmail.com>
2020-07-13 21:44:18 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    private static final String name = "Aid";
+    private static final int year = 2020;
+    public static void main(String[] args) {
+        System.out.printf("Hello! My name is %s.\n",name);
+        System.out.printf("I was created in %d. \n",year);
+    }
+}
